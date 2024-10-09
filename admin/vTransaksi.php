50dc95d1a0718b9d74bd2b5ea378ed288c57ca20
Tegar Oktavianto Simbolon <oktaviantotegar2@gmail.com>
2024-10-10 02:10:43 +0700

    sudah acc

--- admin/vTransaksi.php
+++ admin/vTransaksi.php
@@ -256,6 +256,11 @@ $result = mysqli_query($conn, $query);
                                     <td><?= $row["jenisPemesanan"] ?></td>
                                 </tr>
                                 <tr>
+                                <th>Diskon</th>
+                                <td><?= $row["diskon"] ?>%</td>
+                            </tr>
+
+                                <tr>
                                     <th>Status</th>
                                     <td>
                                         <?php
@@ -321,6 +326,13 @@ $result = mysqli_query($conn, $query);
                                     <th>Pelayanan</th>
                                     <td><?= $row["jenisPemesanan"] ?></td>
                                 </tr>
+                                <tr>
+                                    <th>Diskon</th>
+                                    <td><?= $row["diskon"] ?>%</td>
+                                </tr>
+
+
+
                                 <tr>
                                     <th>Status Sekarang</th>
                                     <td>
